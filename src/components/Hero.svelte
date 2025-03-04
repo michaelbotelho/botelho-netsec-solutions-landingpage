e3d7bec94254358d7affa77ca3603a5bbf2a0a80
Michael Botelho <michaelmbotelho@outlook.com>
2025-03-03 21:39:13 -0500

    Update styles and interactions; work on Hero in Hero.svelte

--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -5,4 +5,13 @@
 </script>
 <SectionWrapper>
     <Header />
+
+    <div class="flex flex-col gap-10 flex-1 items-center justify-center pb-10 md:pb-14">
+        <h2 class="text-4xl sm:text-5xl md:text-6xl lg:text-7xl max-w-[1400px] mx-auto w-full text-center font-semibold">
+            <span class="">Secure Your Network</span><br>
+            with solutions tailored to<br>
+            <span class="text-purple-700 italic ">Your Business</span>
+            <br>
+        </h2>
+    </div>
 </SectionWrapper>
