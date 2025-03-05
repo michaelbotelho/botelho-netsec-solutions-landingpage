fe832e74038dc146878d69f9557623e5c4d4e07f
Michael Botelho <michaelmbotelho@outlook.com>
2025-03-05 06:23:11 -0500

    Work on footer

---
+++ src/components/Footer.svelte
@@ -0,0 +1,60 @@
+<footer class="py-16 sm:py-20 md:py-24 px-4 md:px-8">
+    <div
+        class="max-w-[1400px] mx-auto w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-5 gap-8 text-base"
+    >
+        <div class="flex flex-col gap-4 md:col-span-2">
+            <a href="/" class="flex items-center">
+                <img
+                    src="favicon.png"
+                    alt="Botelho NetSec Solutions"
+                    class="w-10 h-10 mr-4"
+                />
+                <h1 class="text-lg md:text-xl lg:text-2xl font-bold">
+                    Botelho NetSec Solutions
+                </h1>
+            </a>
+            <p class="text-slate-500">&copy;2025 Botelho NetSec Solutions. All rights reserved.</p>
+        </div>
+        <div class="flex flex-col gap-4">
+            <p class="font-bold poppins text-base sm:text-lg">Company</p>
+            <a
+                href=""
+                target="_blank"
+                class="cursor-pointer text-slate-500 [&:hover]:text-slate-400 duration-200"
+            >
+                About
+            </a>
+            <a
+                href=""
+                target="_blank"
+                class="cursor-pointer text-slate-500 [&:hover]:text-slate-400 duration-200"
+            >
+                Contact
+            </a>
+            <a
+                href=""
+                target="_blank"
+                class="cursor-pointer text-slate-500 [&:hover]:text-slate-400 duration-200"
+            >
+                Reviews
+            </a>
+            <a
+                href=""
+                target="_blank"
+                class="cursor-pointer text-slate-500 [&:hover]:text-slate-400 duration-200"
+            >
+                FAQs
+            </a>
+        </div>
+        <div class="flex flex-col gap-4">
+            <p class="font-bold poppins text-base sm:text-lg">Services</p>
+            <a
+                href=""
+                target="_blank"
+                class="cursor-pointer text-slate-500 [&:hover]:text-slate-400 duration-200"
+            >
+                Consulting
+            </a>
+        </div>
+    </div>
+</footer>
